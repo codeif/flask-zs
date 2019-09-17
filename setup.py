1d45f41a7cfa3868be7eea1395f3f74c1d2c4311
codeif <me@codeif.com>
2019-09-17 22:31:44 +0800

    version: 0.0.19

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='flask-zs',
-    version='0.0.18',
+    version='0.0.19',
     description='A helpers for Flask.',
     long_description=readme,
     author='codeif',
